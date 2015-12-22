455e5c16607cd341b0c227e418943fa4c250f9c4
Aysegulkut <aysegulkut53@gmail.com>
2015-12-22 11:51:55 +0200

    Update note.m

--- note.m
+++ note.m
@@ -1,9 +1,9 @@
-function [xx,tt]=note(frekans,vurus) 
-Fs=8192;
-tt=0:1/Fs:((0.25-(1/Fs))/2); 
-zz=0;
-   xx=sin(2*pi*frekans*tt); 
-if length(xx)==length(zz)    
-       zz=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))];
+function [x,t]=note(frekans,vurus) 
+Fs=8192; %örnekleme frekansı
+t=0:1/Fs:((0.25-(1/Fs))/2); %t aralığını tanımladık.
+z=0; %z yi (zarf) 0 a eşitledim.
+   x=sin(2*pi*frekans*t); %çizdirdiğim sinyali x e atadım
+if length(x)==length(z)   %eğer x ve z nin lengthleri eşitse birbirine 
+       z=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))];%zarf değerlerinin matrisini oluşturdum.
 end
 end
