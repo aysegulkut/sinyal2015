1067c6c7cc741c7a1338b837d660f57578e9a179
Aysegulkut <aysegulkut53@gmail.com>
2015-12-22 12:01:35 +0200

    Update notecagirma.m

--- notecagirma.m
+++ notecagirma.m
@@ -1,11 +1,12 @@
-[x,y]=note(53,1/8)
-plot(y,x)
+[x,y]=note(50,1/8)
+plot(y,x)%zarfı plot ile çizdirir
 
-zz1=[linspace(0,1.5,8192*(1/8)*(2/8)),linspace(1.5,1,8192*(1/8)*(1/8)),linspace(1,1,8192*(1/8)*(4/8)),linspace(1,0,8192*(1/8)*(1/8))]
-   plot(y,zz1)
+z1=[linspace(0,1.5,8192*(1/8)*(2/8)),linspace(1.5,1,8192*(1/8)*(1/8)),linspace(1,1,8192*(1/8)*(4/8)),linspace(1,0,8192*(1/8)*(1/8))]
+   plot(y,z1)%zarfın pozitif kısmını çizdirir.
     
-zz2=[linspace(0,-1.5,8192*(1/8)*(2/8)),linspace(-1.5,-1,8192*(1/8)*(1/8)),linspace(-1,-1,8192*(1/8)*(4/8)),linspace(-1,0,8192*(1/8)*(1/8))]
-plot(y,zz2)
-zz3=zz1.*zz2
-carp=x.*zz3
-plot(y,carp)
+z2=[linspace(0,-1.5,8192*(1/8)*(2/8)),linspace(-1.5,-1,8192*(1/8)*(1/8)),linspace(-1,-1,8192*(1/8)*(4/8)),linspace(-1,0,8192*(1/8)*(1/8))]
+plot(y,z2)%zarfın negatif kısmını çizdirir.
+
+z3=z1.*z2 %z1 ile z2 matrisinin çarpımını z3 e atadım.
+carp=x.*z3 %x ile z3 çarpımını carp değişkenine atadım.
+plot(y,carp)%zarflanmış sinyali oluşturdum.
