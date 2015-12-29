eae3d9275f91c21a0f33dca4b10359d0628fa69e
Aysegulkut <aysegulkut53@gmail.com>
2015-12-29 09:39:57 +0200

    Update sentez.m

--- sentez.m
+++ sentez.m
@@ -1,25 +1,25 @@
-fs=8192; %örnekleme frekansını tanımladım
-duraklama=fs*(1/100); %duraklama tanımladım
+fs=8192; %Ã¶rnekleme frekansÄ±nÄ± tanÄ±mladÄ±m
+duraklama=fs*(1/100); %duraklama tanÄ±mladÄ±m
 oktav_degeri=0; %oktav degerinde degisiklik olmama durumu.
 oktavdegis=0; %oktav'a eklenmek istenen deger oktdegis degiskeniyle eklenecek[-1,+1].
-notalar={}; %notalar dizisi oluşturdum
-dosya=fopen('notalar.txt'); %notalar.txt dosyasını okumak açar.
-[nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %notalar.txt deki değerleri okur
-fclose(dosya);%dosyayı kapatır.
+notalar={}; %notalar dizisi oluÅŸturdum
+dosya=fopen('notalar.txt'); %notalar.txt dosyasÄ±nÄ± okumak aÃ§ar.
+[nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %notalar.txt deki deÄŸerleri okur
+fclose(dosya);%dosyayÄ± kapatÄ±r.
 
-if oktav_degeri~=oktavdegis %oktdegis degiskeninde degisiklik yapılması durumunda islenecek sart. 
+if oktav_degeri~=oktavdegis %oktdegis degiskeninde degisiklik yapÄ±lmasÄ± durumunda islenecek sart. 
     for j=1:length(oktav) %text'ten cekilen oktav degerleri dongu icerisinde degistirilecek.
-        oktav(j)=oktav(j)+oktavdegis; %oktavı arttırır
+        oktav(j)=oktav(j)+oktavdegis; %oktavÄ± arttÄ±rÄ±r
     end
 end
 
 for i=1:length(nota)
-f=frek(nota{i},oktav(i)); %notalar.txt deki notaların frekanslarını hesaplar
-[topla,t]=note(f,olcu{i}); %frekans ve olcuye göre sinyal oluşturur.
-t=0:1/fs:olcu{i}-(1/fs); %t aralığı tanımladım.
-notalar{i}=f; %notalar dizisinin ilk elemanına frekans
-notalar{i+1}=duraklama; %ikinci elemanına duraklama süresini atadım.
-plot(t,f,t,duraklama) %frekans ve duraklama süresini çizdirdim.
-plot(t,f,t,echo,t,duraklama) %frekans, echo ve duraklama süresini çizdirdim.
-sound(y,echo)%ses çalmak için
+f=frek(nota{i},oktav(i)); %notalar.txt deki notalarÄ±n frekanslarÄ±nÄ± hesaplar
+[topla,t]=note(f,olcu{i}); %frekans ve olcuye gÃ¶re sinyal oluÅŸturur.
+t=0:1/fs:olcu{i}-(1/fs); %t aralÄ±ÄŸÄ± tanÄ±mladÄ±m.
+notalar{i}=f; %notalar dizisinin ilk elemanÄ±na frekans
+notalar{i+1}=duraklama; %ikinci elemanÄ±na duraklama sÃ¼resini atadÄ±m.
+plot(t,f,t,duraklama) %frekans ve duraklama sÃ¼resini Ã§izdirdim.
+plot(t,f,t,echo,t,duraklama) %frekans, echo ve duraklama sÃ¼resini Ã§izdirdim.
+sound(echo)%ses Ã§almak iÃ§in
 end
