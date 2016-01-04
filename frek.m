8ee151e50829b2dd9997f21202e8369da57ac163
Aysegulkut <aysegulkut53@gmail.com>
2016-01-04 16:44:35 +0200

    frekans fonksiyonunu içerir.

--- frek.m
+++ frek.m
@@ -1,4 +1,4 @@
-function ff=frek(nota,oktav)  %oluþturduðum fonksiyon nota ve oktava göre argüman alacaklar.
+function f=frek(nota,oktav)  %oluþturduðum fonksiyon nota ve oktava göre argüman alacaklar.
 notalar={'Do','Dod', 'Re', 'Mib' ,'Mi' ,'Fa', 'Fad', 'Sol', 'Sold', 'La', 'Sib', 'Si','Sus'}; %nota için tanýmladýðým karakterleri notalar dizisine atadým.
 referans=16.35;  %do notasýnýn 0.oktavdaki deðerini referans olarak aldým.
 p=length(notalar); %p deðiþkenine notalar dizisinin uzunluðunu atadým.
@@ -10,10 +10,10 @@ for i=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým.
        for j=1:p %notalar dizindeki elemanlar için for döngüsü oluþturdum
            if  size(nota)==size(notalar{j}) %argüman olan nota boyutu eþit ise notalar dizisindeki j.elemana
               if nota==notalar{j}%argüman olan nota, notalar dizisindeki j. elemana eþitse
-                  ff=2^i*(2^((j-1)/12)*referans);%frekansý hesaplýyorum
+                  f=2^i*(2^((j-1)/12)*referans);%frekansý hesaplýyorum
                   if size(nota)==size(notalar{13})%argüman olan nota boyutu eþit ise notalar dizisindeki 13.elemana
                   if nota==notalar{13}%argüman olan nota notalar dizisindeki 13. elemana eþitse
-                      ff=0; %frekansý 0 a eþitle.
+                      f=0; %frekansý 0 a eþitle.
                   end
                   end
               end 
